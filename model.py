480276a8521cc8faf106bbc55376ba004480bd4b
Snehangsu De <desnehangsu@gmail.com>
2021-09-06 03:16:10 +0530

    Update Docstring

--- model.py
+++ model.py
@@ -4,9 +4,11 @@ from data import morse
 class MorseConverter:
 
     def __init__(self):
+        """Initilizes the list which the output would be returned"""
         self.output = []
 
     def text_morse(self, code):
+        """Takes in the input and converts that to a morse code"""
         self.output.clear()
         text = list(code)
         for item in text:
@@ -16,6 +18,7 @@ class MorseConverter:
         return " ".join(self.output)
 
     def morse_text(self, code):
+        """Takes in the morse input and converts that to text"""
         self.output.clear()
         text = code.split()
         for item in text:
